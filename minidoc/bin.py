303fe7b238aa8647b32bc6668e7a2c727a06dfd4
dli <ihgazni2010@hotmail.com>
2019-02-09 12:23:45 +0000

    remove old build

--- minidoc/bin.py
+++ minidoc/bin.py
@@ -8,7 +8,7 @@ parser.add_argument('-tst','--test_file', default="code.tst.py",help=".tst.py fi
 parser.add_argument('-codec','--codec', default="utf-8",help=".tst.py file codec")
 parser.add_argument('-still','--still_frames', default="True",help="generate screen shot")
 parser.add_argument('-rows','--rownums', default="30",help="screen height")
-parser.add_argument('-dst','--dst_dir', default="./images",help="screen height")
+parser.add_argument('-dst','--dst_dir', default="./images",help="destination svg dir")
 parser.add_argument('-title','--title', default="Usage",help="parent title")
 parser.add_argument('-tbot','--title_bot', default="=",help="parent title bottom char")
 parser.add_argument('-ebot','--entry_bot', default="-",help="entry title bottom char")
